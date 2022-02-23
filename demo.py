182cf3948906b84e70d199734e09bcda46029672
GuitarChenzy <602974311@qq.com>
2022-02-23 22:33:29 +0800

    get teh pointcould

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+from random import random
 from time import time
 
 
@@ -30,64 +31,26 @@ def get_color(depth):
 # 视差计算
 
 
-def disparity_SGBM(left_image, right_image, down_scale=False):
-
-    # SGBM匹配参数设置
-    if left_image.ndim == 2:
-        img_channels = 1
-    else:
-        img_channels = 3
-    blockSize = 3
-    param = {'minDisparity': 0,
-             'numDisparities': 128,
-             'blockSize': blockSize,
-             'P1': 8 * img_channels * blockSize ** 2,
-             'P2': 32 * img_channels * blockSize ** 2,
-             'disp12MaxDiff': 1,
-             'preFilterCap': 63,
-             'uniquenessRatio': 15,
-             'speckleWindowSize': 100,
-             'speckleRange': 1,
-             'mode': cv.STEREO_SGBM_MODE_SGBM_3WAY
-             }
-
-    # 构建SGBM对象
-    sgbm = cv.StereoSGBM_create(**param)
+# 视差计算
+def sgbm(imgL, imgR):
+    # SGBM参数设置
+    blockSize = 9
+    img_channels = 3
+    stereo = cv.StereoSGBM_create(minDisparity=0,
+                                  numDisparities=96,
+                                  blockSize=blockSize,
+                                  P1=8 * img_channels * blockSize * blockSize,
+                                  P2=32 * img_channels * blockSize * blockSize,
+                                  disp12MaxDiff=1,
+                                  preFilterCap=63,
+                                  uniquenessRatio=10,
+                                  speckleWindowSize=100,
+                                  speckleRange=100,
+                                  mode=cv.STEREO_SGBM_MODE_HH)
     # 计算视差图
-    size = (left_image.shape[1], left_image.shape[0])
-    if down_scale == False:
-        disparity_left = sgbm.compute(left_image, right_image)
-        disparity_right = sgbm.compute(right_image, left_image)
-    else:
-        left_image_down = cv.pyrDown(left_image)
-        right_image_down = cv.pyrDown(right_image)
-        factor = size[0] / left_image_down.shape[1]
-        disparity_left_half = sgbm.compute(left_image_down, right_image_down)
-        disparity_right_half = sgbm.compute(right_image_down, left_image_down)
-        disparity_left = cv.resize(
-            disparity_left_half, size, interpolation=cv.INTER_AREA)
-        disparity_right = cv.resize(
-            disparity_right_half, size, interpolation=cv.INTER_AREA)
-        disparity_left *= factor
-        disparity_right *= factor
-
-    return disparity_left, disparity_right
-
-
-# 立体校正检验----画线
-
-def draw_line2(image1, image2):
-    # 建立输出图像
-    height = max(image1.shape[0], image2.shape[0])
-    width = image1.shape[1] + image2.shape[1]
-    output = np.zeros((height, width), dtype=np.uint8)
-    output[0:image1.shape[0], 0:image1.shape[1]] = image1
-    output[0:image2.shape[0], image1.shape[1]:] = image2
-    for k in range(15):
-        cv.line(output, (0, 50 * (k + 1)), (2 * width, 50 * (k + 1)),
-                (0, 255, 0), thickness=2, lineType=cv.LINE_AA)  # 直线间隔：100
-
-    return output
+    disp = stereo.compute(imgL, imgR)
+    disp = np.divide(disp.astype(np.float32), 16.)  # 除以16得到真实视差图
+    return disp
 
 
 prev_img = None
@@ -98,46 +61,48 @@ cur_R = None
 cur_t = None
 kitti_pose = None
 true_x, true_y, true_z = 0.0, 0.0, 0.0
-points = np.array([0.0, 0.0, 0.0])
+points = list()
 detector = cv.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
 # detector = cv.xfeatures2d.SIFT_create()
-
 lk_params = dict(winSize=(21, 21),
                  # maxLevel = 3,
                  criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01))
 
-
 # err_plt = Mplot2d(xlabel='img id', ylabel='m', title='error')
+
 
 class PinholeCamera:
 
-    def __init__(self, width, height, fx, fy, cx, cy, k1=0.0, k2=0.0, p1=0.0, p2=0.0, k3=0.0):
+    def __init__(self, width, height, fx, fy, cx, cy, b):
         self.width = width
         self.height = height
         self.fx = fx
         self.fy = fy
         self.cx = cx
         self.cy = cy
-        self.distortion = (abs(k1) > 0.0000001)
-        self.d = [k1, k2, p1, p2, k3]
+        self.b = b
         self.K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
 
-cam = PinholeCamera(1241.0, 376.0, 718.8560, 718.8560, 607.1928, 185.2157)
+
+cam = PinholeCamera(1241.0, 376.0, 718.8560, 718.8560,
+                    607.1928, 185.2157, 0.573)
 focal = cam.fx
 pp = (cam.cx, cam.cy)
 
-# with open('/media/czy/DATA/Share/Kitti/poses/00.txt') as f:
-with open('/home/guitarchen/dataset/poses/00.txt') as f:
+with open('/media/czy/DATA/Share/Kitti/poses/00.txt') as f:
+    # with open('/home/guitarchen/dataset/poses/00.txt') as f:
     kitti_pose = f.readlines()
 
-for img_id in range(0, 20):
+for img_id in range(0, 3):
     print("process img: " + str(img_id) + "--------------------------")
-    img = cv.imread('/home/guitarchen/dataset/00/image_0/' +
-                    str(img_id).zfill(6) + '.png', 0)
-    cv.imshow("now img", img)
-    # img = cv.imread('/media/czy/DATA/Share/Kitti/color/00/image_2/' +
+    # img = cv.imread('/home/guitarchen/dataset/00/image_0/' +
     #                 str(img_id).zfill(6) + '.png', 0)
-
+    img = cv.imread('/media/czy/DATA/Share/Kitti/color/00/image_2/' +
+                    str(img_id).zfill(6) + '.png', 0)
+    img_r = cv.imread(
+        '/media/czy/DATA/Share/Kitti/color/00/image_3/' + str(img_id).zfill(6) + '.png', 0)
+    cv.imshow("now img", img)
+    cv.waitKey(-1)
     if img_id == 0:
         feat_points = detector.detect(img)
         out_img = cv.drawKeypoints(
@@ -154,13 +119,14 @@ for img_id in range(0, 20):
         kp2 = kp2[st == 1]
         E, mask = cv.findEssentialMat(
             kp2, kp1, focal=focal, pp=pp, method=cv.RANSAC, prob=0.999, threshold=1.0)
-        _, cur_R, cur_t, mask = cv.recoverPose( E, kp2, kp1, focal=focal, pp=pp)
+        _, cur_R, cur_t, mask = cv.recoverPose(E, kp2, kp1, focal=focal, pp=pp)
         print("cur_R: " + str(cur_R) + "\n" + "cur_t: " + str(cur_t))
         px_ref = kp1
         px_cur = kp2
 
     else:
-        kp2, st, err = cv.calcOpticalFlowPyrLK( prev_img, img, px_ref, None, **lk_params)
+        kp2, st, err = cv.calcOpticalFlowPyrLK(
+            prev_img, img, px_ref, None, **lk_params)
         st = st.reshape(st.shape[0])
         kp1 = px_ref[st == 1]
         kp2 = kp2[st == 1]
@@ -169,7 +135,8 @@ for img_id in range(0, 20):
         _, R, t, mask = cv.recoverPose(E, kp2, kp1, focal=focal, pp=pp)
         print("R: " + str(R) + "\n" + "t: " + str(t))
 
-        projMatr1 = np.array( [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])    # 第一个相机参数
+        projMatr1 = np.array(
+            [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])    # 第一个相机参数
         projMatr2 = np.concatenate((R, t), axis=1)               # 第二个相机参数
         projMatr1 = np.matmul(cam.K, projMatr1)  # 相机内参 相机外参
         projMatr2 = np.matmul(cam.K, projMatr2)
@@ -180,10 +147,12 @@ for img_id in range(0, 20):
         print("depth: " + str(points4D))
 
         line = kitti_pose[img_id - 1].strip().split()
-        x_prev, y_prev, z_prev = float(line[3]), float(line[7]), float(line[11])
+        x_prev, y_prev, z_prev = float(
+            line[3]), float(line[7]), float(line[11])
         line = kitti_pose[img_id].strip().split()
         x, y, z = float(line[3]), float(line[7]), float(line[11])
-        absolute_scale = np.sqrt( (x - x_prev)*(x - x_prev) + (y - y_prev)*(y - y_prev) + (z - z_prev)*(z - z_prev))
+        absolute_scale = np.sqrt(
+            (x - x_prev)*(x - x_prev) + (y - y_prev)*(y - y_prev) + (z - z_prev)*(z - z_prev))
         print("absolute_scale: " + str(absolute_scale))
 
         cur_t = cur_t + absolute_scale * cur_R.dot(t)
@@ -216,9 +185,48 @@ for img_id in range(0, 20):
         # plt.imshow(img[:,:])
         # plt.show()
 
-        lookdispL, lookdispR = disparity_SGBM(prev_img, img)
-        linepic2 = draw_line2(lookdispL, lookdispR)
-        # print(type(lookdispL))
-        plt.imshow(lookdispL)
+        # stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
+        disparity = sgbm(img, img_r)
+        cv.imshow("disparity", disparity/96.0)
+        cv.waitKey(-1)
+        # plt.imshow(disparity,'gray')
+        # plt.show()
+        print(img.shape[1])
+        for v in range(img.shape[0]):
+            for u in range(img.shape[1]):
+                if disparity[v, u] <= 0.0 and disparity[v, u] >= 96.0:
+                    continue
+                point = np.array([0, 0, 0, img[v, u] / 255.0])
+                x = (u-cam.cx) / cam.fx
+                y = (v - cam.cy) / cam.fy
+                depth = cam.fx * cam.b / (disparity[v, u])
+                point[0] = x * depth
+                point[1] = y * depth
+                point[2] = depth
+                points.append(point)
 
     prev_img = img
+# print(str(points))
+
+pangolin.CreateWindowAndBind("Point Cloud Viewer", 1024, 768)
+gl.glEnable(gl.GL_DEPTH_TEST)
+gl.glEnable(gl.GL_BLEND)
+gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+s_cam = pangolin.OpenGlRenderState(
+    pangolin.ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+    pangolin.ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0))
+d_cam = pangolin.CreateDisplay()
+d_cam.SetBounds(0.0, 1.0, 175 / 1024., 1.0, -1024.0 / 768.0)
+d_cam.SetHandler(pangolin.Handler3D(s_cam))
+while pangolin.ShouldQuit() is False:
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+    d_cam.Activate(s_cam)
+    gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+    gl.glPointSize(2)
+    gl.glBegin(gl.GL_POINTS)
+    for p in points:
+            gl.glColor3f(p[3], p[3], p[3])
+            gl.glVertex3d(p[0], p[1], p[2])
+    gl.glEnd()
+    pangolin.FinishFrame()
+    time.sleep(5)
